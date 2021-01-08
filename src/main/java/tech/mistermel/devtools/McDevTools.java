34c53012ffac65fe712480e57c0cd3e475666769
MisterMelDev <melle@mistermel.tech>
2021-01-08 12:54:33 +0100

    Setup project

---
+++ src/main/java/tech/mistermel/devtools/McDevTools.java
@@ -0,0 +1,17 @@
+package tech.mistermel.devtools;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class McDevTools extends JavaPlugin {
+	
+	@Override
+	public void onEnable() {
+		
+	}
+	
+	@Override
+	public void onDisable() {
+		
+	}
+	
+}
